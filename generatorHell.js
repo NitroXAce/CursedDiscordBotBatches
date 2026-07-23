884502dbdf148c9ed4a136c5bf7d2eb406388184
NitroXAce <gearheadgareth@outlook.com>
2026-07-23 14:16:23 -0400

    incomplete update - worked on reiterating generator hell

--- generatorHell.js
+++ generatorHell.js
@@ -1,20 +1,151 @@
 //WORK IN PROGRESS
 //INCOMPLETE!!
+(function* main(arr){
+    for (const item of arr());
+})([
 
-import {
-    Client,
-    Collection,
-    GatewayIntentBits,
-    SlashCommandBuilder,
-    EmbedBuilder,
-    Events,
-    REST,
-    Routes,
-} from "discord.js";
-import fs from "fs";
-import webSocket from "ws";
-import tokens from "./config.json" with { type: "json" };
-import { userInfo } from "os";
+    //get the required modules
+    require("discord.js"),
+    require('fs'),
+    require('./config.json').tokens,
+    require('ws'),
+
+    // 4 :: edit file
+    function* Editor(user,callback){
+
+        //extract value as a return for this function
+        (function* Extract(Value){
+            yield this[1].writeFileSync("./users.json", JSON.stringify((
+                
+                //internalized manipulation of a oneliner user data
+                function* manipulate(value){
+                    value = JSON.parse(this[1].readFileSync("./users.json"));
+                    
+                    //"build" an object for user undefined
+                    if(!(user in value))
+                        for (key of function* keys(){
+                            yield "points";
+                            yield "chat";
+                            yield "freebie";
+                        }()) value[user][key] = 0;
+
+                    //run all callback generator yields
+                    for (key of callback(value[user]));
+                    Value = value;
+                    yield value;
+                }().next().value
+            )));
+            yield Value;
+        })().next().value;
+    },
+
+    // 5 :: message event
+    function* Message(embeds){
+        yield "ephemeral";
+        yield true;
+        yield "content";
+        yield "";
+        yield "embeds";
+        yield [embeds];
+    },
+
+    // 6 :: footer for all embeds
+    function* Footer(timeBegin, timeEnd){
+        yield "text";
+        yield `Request done in ${Math.ceil((timeEnd - timeBegin) * 1000) / 1000}ms`;
+        yield "iconURL";
+        yield "https://tenor.com/view/clock-gif-14609778";
+    },
+
+    // 7 :: all the commands are created here
+    function* Commands(){
+        yield function* Ping(){
+            yield new this[0].SlashCommandBuilder()
+                .setName("ping")
+                .setDescription("Replies with Pong!");
+            yield function* Execute(interaction){
+                yield new this[0].EmbedBuilder()
+                    .setTitle("Pong!")
+                    .setDescription("Pong!")
+                    .setColor("Green");
+            };
+        };
+        yield function* Freebie(){
+            yield new this[0].SlashCommandBuilder()
+                .setName("freebie")
+                .setDescription("Replies with a free item!");
+            yield function* Execute(interaction){
+
+                //temp variable to store the user data
+                yield* (function* tempUserData(result){
+                    result = this[3](interaction.user.id, function* cb(jsonUser){
+                        if(Date.now() > jsonUser.freebie + 3600000){
+                            jsonUser.freebie = Date.now();
+                            jsonUser.before = jsonUser.points;
+                            jsonUser.points += Math.round(Math.random() * 1000) + 500;
+                            jsonUser.bool = true;
+                        }
+                    }).next().value;
+
+                    yield new this[0].EmbedBuilder()
+                        .setTitle("Freebie")
+                        .setDescription(
+                            result.bool
+                                ? `You earned: ${result.points - result.before}pts`
+                                : "You already got a free item this hour!"
+                        )
+                        .setColor(result.bool ? "Green" : "Red");
+                })();
+            }
+        };
+    },
+
+    // 8 :: deploy commands to discord
+    function* Deploy({commandList, clientId, guildId}){
+        if(commandList && clientId && guildId)
+            new this[0].REST({version: "10"})
+                .setToken(this[2].djs)
+                .put(this[0].Routes.applicationGuildCommands(clientId, guildId), {body: commandList})
+                .then(comms => console.log(`Successfully reloaded ${comms.length} application (/) commands.`))
+                .catch(console.log);
+        else throw new Error("Failed to deploy commands");
+    },
+
+    // 9 :: bot events to fire and execute commands
+    function* BotEvents(){
+        yield function* Ready(){
+            yield true;
+            yield this[0].Events.ClientReady;
+            yield function* Execute(client){
+                yield console.log(`Ready! Logged in as ${client.user.tag}`);
+            };
+        };
+        yield function* InteractionCreate(){
+            yield false;
+            yield this[0].Events.InteractionCreate;
+            yield function* Execute(interaction){
+                if(!interaction.isChatInputCommand()) return;
+                for(command of this[7]())
+                    for(parts of command()){
+                        if(
+                            'name' in parts &&
+                            interaction.commandName === parts.name
+                        ) continue;
+                        else if (typeof parts === "function"){
+                            interaction
+                                .reply(
+                                    this[5](
+                                        parts(interaction)
+                                    )
+                                    .next()
+                                    .value
+                                )
+                        }
+                        
+
+
+]).next();
+
 
 function* editor(user, callback) {
     let obj = JSON.parse(fs.readFileSync("./users.json"));
